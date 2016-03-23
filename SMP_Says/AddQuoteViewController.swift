6e62a6ea5f8b8751f0bf99f4fd08f21afc2f6181
Evan Edge <evanjedge@gmail.com>
2016-03-22 17:56:13 -0700

    add quote function now works.

--- SMP_Says/AddQuoteViewController.swift
+++ SMP_Says/AddQuoteViewController.swift
@@ -13,7 +13,51 @@ import SwiftyJSON
 class AddQuoteViewController: UIViewController {
 
     
-    @IBOutlet weak var quoteTextView: UITextField!
+    @IBOutlet weak var schoolField: UITextField!
+    @IBOutlet weak var quoteField: UITextField!
+    @IBOutlet weak var subjectField: UITextField!
+    @IBOutlet weak var professorField: UITextField!
+    
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.schoolField.becomeFirstResponder()
+    }
+    
+    
+    @IBAction func onSubmitButtonClick(sender: AnyObject) {
+        
+        let school: String = schoolField.text!
+        let quote: String = quoteField.text!
+        let subject: String = subjectField.text!
+        let professor: String = professorField.text!
+        
+        addQuote(school, quoteText: quote, subjectText: subject, professorText: professor)
+    
+    }
+    
+    func addQuote(schoolText: String, quoteText: String, subjectText: String, professorText: String) {
+        
+        let parameters = [
+            "quotation": quoteText,
+            "school": schoolText,
+            "professor": professorText,
+            "subject": subjectText
+        ]
+
+        let url = "http://www.smpsays-api.xyz/RUEf2i15kex8nXhmJxCW2ozA5SNIyfLn/save/quote"
+        Alamofire.request(.POST, url, parameters: parameters, encoding: .JSON)
+            .response { request, response, data, error in
+                print(request)
+                print(response)
+                print(data)
+                print(error)
+            
+        }
+
+    }
     
     
 }
